3663b73c6dc61a00f60f57a10e36393bde8b2b8f
Leandro Martins <leandro.martins.85@gmail.com>
2021-11-03 12:06:05 -0300

    Css fix + image

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,9 @@ import { useState } from 'react';
 import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
 
-
-
 import './styles/global.scss';
 
 import './styles/sidebar.scss';
-
-
-
 
 export function App() {  
   const [selectedGenreId, setSelectedGenreId] = useState(1);
